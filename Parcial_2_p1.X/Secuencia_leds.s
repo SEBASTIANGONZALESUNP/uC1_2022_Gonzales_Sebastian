63a84e9fe3702506a113a6bcf0ac5b6b2ffdd26d
SEBASTIANGONZALESUNP <1332021048@alumnos.unp.edu.pe>
2023-01-30 02:04:29 -0500

    Examen Parcial - Ejercicio 2 (secuencia de Leds)

---
+++ Parcial_2_p1.X/Secuencia_leds.s
@@ -0,0 +1,256 @@
+;Parcial-2_Secuencia_leds
+;________________________________________________________________________________
+;________________________________________________________________________________
+; @file: P1-Corrimiento_Leds.s
+; @brief:
+;   Este es un programa para que al presionar el botón de placa se ejecute un
+;   patrón de encendidos de leds previamente establecido en los pines del puerto
+;   C de la placa.
+;
+;   La secuencia se detiene al presionar otro pulsador externo conectado en el
+;    pin RB4 o hasta que el número de repeticiones sea 5.
+;   Otro pulsador externo conectado al RF2 reinicia toda la secuencia y apaga los
+;   leds.
+;   Mientras no se active ninguna interrupción, el programa principal, realice un
+;   toggle del led de la placa cada 500 ms.
+; @date: 29/01/2023
+; @author: Gonzales Alvarado Sebastian
+; @ide	MPLAB X IDE v6.00
+;_______________________________________________________________________________
+;_______________________________________________________________________________
+    
+PROCESSOR 18F57Q84
+#include "Bit_Config.inc"   /*config statements should precede project file includes.*/
+#include <xc.inc>
+    
+PSECT resetVect,class=CODE,reloc=2
+resetVect:
+    goto Main
+    
+PSECT ISRVectLowPriority,class=CODE,reloc=2
+ISRVectLowPriority:
+    BTFSS   PIR1,0,0	; ¿Se ha producido la INT0?
+    GOTO    Exit0
+Leds_on:
+    BCF	    PIR1,0,0	; limpiamos el flag de INT0
+    GOTO    Reload
+Exit0:
+    RETFIE
+
+PSECT ISRVectHighPriority,class=CODE,reloc=2
+ISRVectHighPriority:
+    BTFSS   PIR10,0,0	; ¿Se ha producido la INT0?
+    GOTO    ISRVectHighPrioritya
+Leds:
+    BCF	    PIR10,0,0	; limpiamos el flag de INT2
+    GOTO    exit
+Exit:
+    RETFIE
+
+
+PSECT udata_acs
+contador1:  DS 1	    
+contador2:  DS 1
+contador3:  DS 1
+offset:	    DS 1
+offset1:    DS 1
+counter:    DS 1
+counter1:   DS 1 
+    
+PSECT CODE    
+Main:
+    CALL    Config_OSC,1
+    CALL    Config_Port,1
+    CALL    Config_PPS,1
+    CALL    Config_INT0_INT1_INT2,1
+    GOTO    Toggle
+    
+    
+Toggle:
+   BTFSC   PIR10,0,0	; ¿Se ha producido la INT0?
+   GOTO	   ISRVectHighPriority
+   BTG	   LATF,3,0
+   CALL    Delay_250ms,1
+   CALL    Delay_250ms,1
+   BTG	   LATF,3,0
+   CALL    Delay_250ms,1
+   CALL    Delay_250ms,1
+   goto	   Toggle
+
+Loop:
+    BSF	    LATF,3,0	    ;toggle off
+    BANKSEL PCLATU
+    MOVLW   low highword(Table)
+    MOVWF   PCLATU,1
+    MOVLW   high(Table)
+    MOVWF   PCLATH,1
+    RLNCF   offset,0,0
+    CALL    Table
+    MOVWF   LATC,0
+    CALL    Delay_250ms,1
+    DECFSZ  counter,1,0
+    GOTO    Next_Seq
+    
+ Verifica:
+    DECFSZ  counter1,1,0
+    GOTO    Reload2
+    Goto    exit
+    
+ Next_Seq:
+    INCF    offset,1,0
+    GOTO    Loop
+    
+Reload:
+    MOVLW   0x05	
+    MOVWF   counter1,0	; carga del contador con el numero de offsets
+    MOVLW   0x00	
+    MOVWF   offset,0	; definimos el valor del offset inicial
+    
+Reload2:
+    MOVLW   0x0A	
+    MOVWF   counter,0	; carga del contador con el numero de offsets
+    MOVLW   0x00	
+    MOVWF   offset,0	; definimos el valor del offset inicial
+    GOTO    Loop  
+    
+    
+Config_OSC:
+    ;Configuracion del Oscilador Interno a una frecuencia de 4MHz
+    BANKSEL OSCCON1
+    MOVLW   0x60    ;seleccionamos el bloque del osc interno(HFINTOSC) con DIV=1
+    MOVWF   OSCCON1,1 
+    MOVLW   0x02    ;seleccionamos una frecuencia de Clock = 4MHz
+    MOVWF   OSCFRQ,1
+    RETURN
+   
+Config_Port:	
+    ;Config Led
+    BANKSEL PORTF
+    CLRF    PORTF,1	
+    BSF	    LATF,3,1
+    BSF	    LATF,2,1
+    CLRF    ANSELF,1	
+    BCF	    TRISF,3,1
+    BCF	    TRISF,2,1
+    
+    ;Config User Button
+    BANKSEL PORTA
+    CLRF    PORTA,1	
+    CLRF    ANSELA,1	
+    BSF	    TRISA,3,1	
+    BSF	    WPUA,3,1
+    
+    ;Config Ext Button
+    BANKSEL PORTB
+    CLRF    PORTB,1	
+    CLRF    ANSELB,1	
+    BSF	    TRISB,4,1	
+    BSF	    WPUB,4,1
+    
+    ;Config Ext Button2
+    BANKSEL PORTF
+    CLRF    PORTF,1	
+    CLRF    ANSELF,1	
+    BSF	    TRISF,2,1	
+    BSF	    WPUF,2,1
+    
+    ;Config PORTC
+    BANKSEL PORTC
+    CLRF    PORTC,1	
+    CLRF    LATC,1	
+    CLRF    ANSELC,1	
+    CLRF    TRISC,1
+    RETURN
+    
+Config_PPS:
+    ;Config INT0
+    BANKSEL INT0PPS
+    MOVLW   0x03
+    MOVWF   INT0PPS,1	; INT0 --> RA3
+    
+    ;Config INT1
+    BANKSEL INT1PPS
+    MOVLW   0x0C
+    MOVWF   INT1PPS,1	; INT1 --> RB4
+    
+    ;Config INT2
+    BANKSEL INT2PPS
+    MOVLW   0x2A
+    MOVWF   INT2PPS,1
+    
+    RETURN
+
+Config_INT0_INT1_INT2:
+    ;Configuracion de prioridades
+    BSF	INTCON0,5,0 ; INTCON0<IPEN> = 1 -- Habilitamos las prioridades
+    BANKSEL IPR1
+    BCF	IPR1,0,1    ; IPR1<INT0IP> = 0 --  INT0 de baja prioridad
+    BSF	IPR6,0,1    ; IPR6<INT1IP> = 0 --  INT1 de alta prioridad
+    BSF	IPR10,0,1   ; IPR1<INT2IP> = 1 --  INT2 de alta prioridad
+    
+    ;Config INT0
+    BCF	INTCON0,0,0 ; INTCON0<INT0EDG> = 0 -- INT0 por flanco de bajada
+    BCF	PIR1,0,0    ; PIR1<INT0IF> = 0 -- limpiamos el flag de interrupcion
+    BSF	PIE1,0,0    ; PIE1<INT0IE> = 1 -- habilitamos la interrupcion ext0
+    
+    ;Config INT1
+    BCF	INTCON0,1,0 ; INTCON0<INT1EDG> = 0 -- INT1 por flanco de bajada
+    BCF	PIR6,0,0    ; PIR6<INT1IF> = 0 -- limpiamos el flag de interrupcion
+    BSF	PIE6,0,0    ; PIE6<INT1IE> = 1 -- habilitamos la interrupcion ext1
+    
+    ;Config INT2
+    BCF	INTCON0,2,0 ; INTCON0<INT2EDG> = 0 -- INT2 por flanco de bajada
+    BCF	PIR10,0,0    ; PIR10<INT2IF> = 0 -- limpiamos el flag de interrupcion
+    BSF	PIE10,0,0    ; PIE10<INT2IE> = 1 -- habilitamos la interrupcion ext1
+    
+    ;Habilitacion de interrupciones
+    BSF	INTCON0,7,0 ; INTCON0<GIE/GIEH> = 1 -- habilitamos las interrupciones de forma global y de alta prioridad
+    BSF	INTCON0,6,0 ; INTCON0<GIEL> = 1 -- habilitamos las interrupciones de baja prioridad
+    RETURN
+
+Table:
+    ADDWF   PCL,1,0
+    RETLW   10000001B	; offset: 0
+    RETLW   01000010B	; offset: 1
+    RETLW   00100100B	; offset: 2
+    RETLW   00011000B	; offset: 3
+    RETLW   00000000B	; offset: 4 -> se apagan todos
+    RETLW   00011000B	; offset: 5
+    RETLW   00100100B	; offset: 6
+    RETLW   01000010B	; offset: 7
+    RETLW   10000001B	; offset: 8
+    RETLW   00000000B	; offset: 9
+    
+    
+ISRVectHighPrioritya:
+    BTFSS   PIR6,0,0	; ¿Se ha producido la INT0?
+    GOTO    Exit
+    GOTO    Toggle
+    
+;Delay_250ms
+Delay_250ms:			;  CALL --> 2Tcy
+    MOVLW   141			;  1Tcy, (K2) 
+    MOVWF   contador2,0		;  1Tcy
+    NOP				;  1Tcy
+Loop_Ext8:				  
+    MOVLW   196			;  1Tcy, (K1).
+    MOVWF   contador1,0		;  1Tcy
+Loop_Int8:
+    NOP				;  (K1*K2)*1Tcy
+    NOP				;  (K1*K2)*1Tcy
+    NOP				;  (K1*K2)*1Tcy
+    NOP				;  (K1*K2)*1Tcy
+    NOP				;  (K1*K2)*1Tcy
+    NOP				;  (K1*K2)*1Tcy
+    DECFSZ  contador1,1,0	;  (K1-1)*K2*1Tcy + K2*3Tcy
+    GOTO    Loop_Int8		;  (K1-1)*K2*2Tcy
+    NOP				;  (K2)* Tcy
+    NOP				;  (K2)* Tcy
+    NOP				;  (K2)* Tcy
+    NOP				;  (K2)* Tcy
+    DECFSZ  contador2,1,0	;  (K2-1)*1Tcy + 3Tcy 
+    GOTO    Loop_Ext8		;  (K2-1)*2Tcy
+    RETURN		        ; 2Tcy
+    
+exit:     
+End resetVect
